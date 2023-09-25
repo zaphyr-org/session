32bfe507cf3d22e8748859522d77252a6fbe6dcb
merloxx <hello@merloxx.it>
2023-09-25 15:56:58 +0200

    New: Added FileHandler class

---
+++ src/Handler/FileHandler.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\Session\Handler;
+
+use SessionHandlerInterface;
+
+/**
+ * @author merloxx <merloxx@zaphyr.org>
+ */
+class FileHandler implements SessionHandlerInterface
+{
+    /**
+     * @var string
+     */
+    protected string $storage;
+
+    /**
+     * @param string $path
+     * @param int    $minutes
+     */
+    public function __construct(string $path, protected int $minutes = 60)
+    {
+        $this->storage = rtrim($path, '\/') . '/';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function open(string $path, string $name): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read(string $id): string|false
+    {
+        $path = $this->storage . $id;
+
+        if (is_file($path) && filemtime($path) >= strtotime('-' . $this->minutes . ' minutes')) {
+            return file_get_contents($this->storage . $id) ?: false;
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write(string $id, string $data): bool
+    {
+        return file_put_contents($this->storage . $id, $data, LOCK_EX) !== false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function destroy(string $id): bool
+    {
+        $path = $this->storage . $id;
+
+        return is_file($path) && unlink($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function gc(int $max_lifetime): int|false
+    {
+        $files = glob($this->storage . '*');
+        $deletedFiles = 0;
+
+        if (is_array($files)) {
+            foreach ($files as $file) {
+                if (filemtime($file) + $max_lifetime < time()) {
+                    unlink($file);
+                    $deletedFiles++;
+                }
+            }
+        }
+
+        return $deletedFiles > 0 ? $deletedFiles : false;
+    }
+}
---
+++ tests/Handler/FileHandlerTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\SessionTests\Handler;
+
+use PHPUnit\Framework\TestCase;
+use Zaphyr\Session\Handler\FileHandler;
+use Zaphyr\Utils\File;
+
+class FileHandlerTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    protected string $tempDir;
+
+    /**
+     * @var string
+     */
+    protected string $tempFile;
+
+    /**
+     * @var string
+     */
+    protected string $tempFileId;
+
+    /**
+     * @var FileHandler
+     */
+    protected FileHandler $fileHandler;
+
+    public function setUp(): void
+    {
+        $this->tempFileId = '1';
+        $this->tempDir = __DIR__ . '/session/';
+        $this->tempFile = $this->tempDir . $this->tempFileId;
+
+        File::createDirectory($this->tempDir);
+        $file = fopen($this->tempFile, 'w');
+
+        if ($file !== false) {
+            fwrite($file, 'foo');
+            fclose($file);
+        }
+
+        $this->fileHandler = new FileHandler($this->tempDir);
+    }
+
+    public function tearDown(): void
+    {
+        File::deleteDirectory($this->tempDir);
+        unset($this->fileHandler);
+    }
+
+    /* ------------------------------------------
+     * OPEN
+     * ------------------------------------------
+     */
+
+    public function testOpenReturnsTrue(): void
+    {
+        self::assertTrue($this->fileHandler->open(__DIR__, 'foo'));
+    }
+
+    /* ------------------------------------------
+     * CLOSE
+     * ------------------------------------------
+     */
+
+    public function testCloseReturnsTrue(): void
+    {
+        self::assertTrue($this->fileHandler->close());
+    }
+
+    /* ------------------------------------------
+     * READ
+     * ------------------------------------------
+     */
+
+    public function testReadReturnsFileContent(): void
+    {
+        self::assertEquals('foo', $this->fileHandler->read($this->tempFileId));
+    }
+
+    public function testReadReturnsEmptyStringWhenFileDoesNotExists(): void
+    {
+        self::assertEquals('', $this->fileHandler->read('nope'));
+    }
+
+    /* ------------------------------------------
+     * WRITE
+     * ------------------------------------------
+     */
+
+    public function testWriteToExistingFile(): void
+    {
+        self::assertTrue($this->fileHandler->write($this->tempFileId, $expected = 'bar'));
+        self::assertEquals($expected, $this->fileHandler->read($this->tempFileId));
+    }
+
+    public function testWriteCreatesNewFileWhenNotExists(): void
+    {
+        self::assertTrue($this->fileHandler->write($file = 'create', $expected = 'bar'));
+        self::assertEquals($expected, $this->fileHandler->read($file));
+
+        unlink($this->tempDir . $file);
+    }
+
+    /* ------------------------------------------
+     * DESTROY
+     * ------------------------------------------
+     */
+
+    public function testDestroy(): void
+    {
+        $this->fileHandler->write($tempFileId = '2', 'baz');
+
+        self::assertFileExists($tempFile = $this->tempDir . $tempFileId);
+        self::assertTrue($this->fileHandler->destroy($tempFileId));
+        self::assertFileDoesNotExist($tempFile);
+    }
+
+    public function testDestroyReturnsFalseWhenFileDoesNotExists(): void
+    {
+        self::assertFalse($this->fileHandler->destroy('999'));
+    }
+
+    /* ------------------------------------------
+     * GARBAGE COLLECTOR
+     * ------------------------------------------
+     */
+
+    public function testGc(): void
+    {
+        self::assertFalse($this->fileHandler->gc(0));
+    }
+}
