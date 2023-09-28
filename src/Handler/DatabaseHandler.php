8840aa9ff5cf90a5df4aabbb05e5f52f5c79a184
merloxx <hello@merloxx.it>
2023-09-28 11:46:05 +0200

    New: Added DatabaseHandler class

---
+++ src/Handler/DatabaseHandler.php
@@ -0,0 +1,162 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\Session\Handler;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
+use SessionHandlerInterface;
+use Throwable;
+
+/**
+ * @author merloxx <merloxx@zaphyr.org>
+ */
+class DatabaseHandler implements SessionHandlerInterface
+{
+    /**
+     * @var string
+     */
+    protected readonly string $table;
+
+    /**
+     * @var string
+     */
+    protected readonly string $idColumn;
+
+    /**
+     * @var string
+     */
+    protected readonly string $dataColumn;
+
+    /**
+     * @var string
+     */
+    protected readonly string $timeColumn;
+
+    /**
+     * @param Connection            $connection
+     * @param array<string, string> $dbOptions
+     * @param int $minutes
+     */
+    public function __construct(
+        protected Connection $connection,
+        array $dbOptions = [],
+        protected int $minutes = 60
+    ) {
+        $this->table = $dbOptions['table'] ?? 'sessions';
+        $this->idColumn = $dbOptions['idColumn'] ?? 'id';
+        $this->dataColumn = $dbOptions['dataColumn'] ?? 'data';
+        $this->timeColumn = $dbOptions['timeColumn'] ?? 'time';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function open(string $path, string $name): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read(string $id): string|false
+    {
+        try {
+            $qb = $this->connection->createQueryBuilder()
+                ->select($this->dataColumn)
+                ->from($this->table)
+                ->where($this->idColumn . ' = :id')
+                ->andWhere($this->timeColumn . ' <= :time')
+                ->setParameter('id', $id)
+                ->setParameter('time', time() + ($this->minutes * 60), ParameterType::INTEGER);
+
+            return base64_decode($qb->fetchOne()) ?: false;
+        } catch (Throwable) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write(string $id, string $data): bool
+    {
+        $this->connection->beginTransaction();
+
+        try {
+            $queryBuilder = $this->connection->createQueryBuilder();
+
+            $queryBuilder
+                ->delete($this->table)
+                ->where($this->idColumn . ' = :id')
+                ->setParameter('id', $id)
+                ->executeStatement();
+
+            $queryBuilder
+                ->insert($this->table)
+                ->values([
+                    $this->idColumn => ':id',
+                    $this->dataColumn => ':data',
+                    $this->timeColumn => ':time',
+                ])
+                ->setParameter('id', $id)
+                ->setParameter('data', base64_encode($data))
+                ->setParameter('time', time(), ParameterType::INTEGER)
+                ->executeStatement();
+
+            $this->connection->commit();
+
+            return true;
+        } catch (Throwable $e) {
+            $this->connection->rollBack();
+
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function destroy(string $id): bool
+    {
+        try {
+            $this->connection
+                ->createQueryBuilder()
+                ->delete($this->table)
+                ->where($this->idColumn . ' = :id')
+                ->setParameter('id', $id)
+                ->executeStatement();
+
+            return true;
+        } catch (Throwable) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function gc(int $max_lifetime): int|false
+    {
+        try {
+            return $this->connection
+                ->createQueryBuilder()
+                ->delete($this->table)
+                ->where($this->timeColumn . ' <= :time')
+                ->setParameter('time', time() - $max_lifetime, ParameterType::INTEGER)
+                ->executeStatement();
+        } catch (Throwable) {
+            return false;
+        }
+    }
+}
---
+++ tests/Handler/DatabaseHandlerTest.php
@@ -0,0 +1,347 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\SessionTests\Handler;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Zaphyr\Session\Handler\DatabaseHandler;
+
+class DatabaseHandlerTest extends TestCase
+{
+    /**
+     * @var Connection&MockObject
+     */
+    protected Connection&MockObject $connectionMock;
+
+    /**
+     * @var MockObject&QueryBuilder
+     */
+    protected QueryBuilder&MockObject $queryBuilderMock;
+
+    /**
+     * @var DatabaseHandler
+     */
+    protected DatabaseHandler $dbHandler;
+
+    /**
+     * @var int
+     */
+    protected int $minutes = 60;
+
+    public function setUp(): void
+    {
+        $this->connectionMock = $this->createMock(Connection::class);
+        $this->queryBuilderMock = $this->createMock(QueryBuilder::class);
+        $this->dbHandler = new DatabaseHandler($this->connectionMock, minutes: $this->minutes);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->connectionMock, $this->queryBuilderMock, $this->dbHandler);
+    }
+
+    /* -------------------------------------------------
+     * OPEN
+     * -------------------------------------------------
+     */
+
+    public function testOpenReturnsTrue(): void
+    {
+        $this->assertTrue($this->dbHandler->open('', ''));
+    }
+
+    /* -------------------------------------------------
+     * CLOSE
+     * -------------------------------------------------
+     */
+
+    public function testCloseReturnsTrue(): void
+    {
+        $this->assertTrue($this->dbHandler->close());
+    }
+
+    /* -------------------------------------------------
+     * READ
+     * -------------------------------------------------
+     */
+
+    public function testRead(): void
+    {
+        $sessionData = 'data';
+
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('select')
+            ->with('data')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('from')
+            ->with('sessions')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('where')
+            ->with('id = :id')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('andWhere')
+            ->with('time <= :time')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::exactly(2))
+            ->method('setParameter')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('fetchOne')
+            ->willReturn(base64_encode($sessionData));
+
+        self::assertEquals($sessionData, $this->dbHandler->read('id'));
+    }
+
+    public function testReadReturnsFalseWhenNoEntryFound(): void
+    {
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('select')
+            ->with('data')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('from')
+            ->with('sessions')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('where')
+            ->with('id = :id')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('andWhere')
+            ->with('time <= :time')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::exactly(2))
+            ->method('setParameter')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('fetchOne')
+            ->willReturn(false);
+
+        self::assertFalse($this->dbHandler->read('id'));
+    }
+
+    public function testReadReturnsFalseOnThrowable(): void
+    {
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willThrowException(new Exception());
+
+        $this->assertFalse($this->dbHandler->read('id'));
+    }
+
+    /* -------------------------------------------------
+     * WRITE
+     * -------------------------------------------------
+     */
+
+    public function testWrite(): void
+    {
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('beginTransaction');
+
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('delete')
+            ->with('sessions')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('where')
+            ->with('id = :id')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('insert')
+            ->with('sessions')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('values')
+            ->with([
+                'id' => ':id',
+                'data' => ':data',
+                'time' => ':time',
+            ])
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::exactly(4))
+            ->method('setParameter')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::exactly(2))
+            ->method('executeStatement');
+
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('commit');
+
+        self::assertTrue($this->dbHandler->write('id', 'data'));
+    }
+
+    public function testWriteReturnsFalseOnThrowable(): void
+    {
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willThrowException(new Exception());
+
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('rollBack');
+
+        $this->assertFalse($this->dbHandler->write('id', 'data'));
+    }
+
+    /* -------------------------------------------------
+     * DESTROY
+     * -------------------------------------------------
+     */
+
+    public function testDestroy(): void
+    {
+        $sessionId =  'id';
+
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('delete')
+            ->with('sessions')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('where')
+            ->with('id = :id')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('setParameter')
+            ->with('id', $sessionId)
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('executeStatement');
+
+        self::assertTrue($this->dbHandler->destroy($sessionId));
+    }
+
+    public function testDestroyReturnsFalseOnThrowable(): void
+    {
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willThrowException(new Exception());
+
+        $this->assertFalse($this->dbHandler->destroy('id'));
+    }
+
+    /* -------------------------------------------------
+     * GARBAGE COLLECTOR
+     * -------------------------------------------------
+     */
+
+    public function testGc(): void
+    {
+        $affectedRows = 1;
+        $maxLifetime = 0;
+
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('delete')
+            ->with('sessions')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('where')
+            ->with('time <= :time')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('setParameter')
+            ->with('time', time() - $maxLifetime, ParameterType::INTEGER)
+            ->willReturn($this->queryBuilderMock);
+
+        $this->queryBuilderMock
+            ->expects(self::once())
+            ->method('executeStatement')
+            ->willReturn($affectedRows);
+
+        self::assertEquals($affectedRows, $this->dbHandler->gc($maxLifetime));
+    }
+
+    public function testGcReturnsFalseOnThrowable(): void
+    {
+        $this->connectionMock
+            ->expects(self::once())
+            ->method('createQueryBuilder')
+            ->willThrowException(new Exception());
+
+        $this->assertFalse($this->dbHandler->gc(0));
+    }
+}
